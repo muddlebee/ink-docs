2a8ba517c0d5f17287f08b2321ff7bab574927a8
Sam Ruberti <samruberti@gmail.com>
2023-02-14 11:58:31 -0500

    Add ink!ubator page

---
+++ docs/inkubator/overview.md
@@ -0,0 +1,9 @@
+---
+title: Overview
+hide_title: true
+slug: /ubator-overview
+---
+
+import { Hero } from "../../src/components/pg-inkubator";
+
+<Hero withLinkCTA className="mt-16" />
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,27 +10,32 @@ module.exports = {
   projectName: 'ink-docs',
   stylesheets: [
     'fonts/fonts.css',
-    'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Montserrat:wght@400;500;700&display=swap'
+    'https://fonts.googleapis.com/css2?family=Fredoka+One&family=Montserrat:wght@400;500;700&display=swap',
   ],
   themes: [
     [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
+      require.resolve('@easyops-cn/docusaurus-search-local'),
       {
         indexPages: true,
-      }
-    ]
+      },
+    ],
   ],
   themeConfig: {
     prism: {
       theme: require('prism-react-renderer/themes/duotoneDark'),
-      additionalLanguages: ['rust', 'json']
+      additionalLanguages: ['rust', 'json'],
     },
     colorMode: {
       defaultMode: 'dark',
-      disableSwitch: false
+      disableSwitch: false,
     },
     navbar: {
       title: '',
+      logo: {
+        alt: 'ink!',
+        src: 'img/text-black.svg',
+        srcDark: '/img/text-white.svg',
+      },
       logo: {
         alt: 'ink!',
         src: 'img/text-black.svg',
@@ -57,39 +62,45 @@ module.exports = {
               label: 'Spanish',
               target: '_parent',
             },
-          ]
-        }
+          ],
+        },
       ],
     },
   },
   presets: [
-    ['@docusaurus/preset-classic', {
-      docs: {
-        sidebarPath: require.resolve('./sidebars.js'),
-        editUrl: 'https://github.com/paritytech/ink-docs/edit/master/',
-        routeBasePath: '/',
-        lastVersion: 'current',
-        versions: {
-          current: {
-            label: '4.0-rc',
-            path: '',
-            banner: 'none',
-          },
-          '3.x': {
-            label: '3.x',
-            path: '3.x',
-            banner: 'none',
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl: 'https://github.com/paritytech/ink-docs/edit/master/',
+          routeBasePath: '/',
+          lastVersion: 'current',
+          versions: {
+            current: {
+              label: '4.0-rc',
+              path: '',
+              banner: 'none',
+            },
+            '3.x': {
+              label: '3.x',
+              path: '3.x',
+              banner: 'none',
+            },
           },
         },
+        blog: {
+          showReadingTime: true,
+          editUrl: 'https://github.com/paritytech/ink-docs/edit/master/',
+        },
+        theme: {
+          customCss: [
+            require.resolve('./src/css/custom.css'),
+            require.resolve('./src/css/faucet.css'),
+          ],
+        },
       },
-      blog: {
-        showReadingTime: true,
-        editUrl: 'https://github.com/paritytech/ink-docs/edit/master/',
-      },
-      theme: {
-        customCss: [require.resolve('./src/css/custom.css'), require.resolve('./src/css/faucet.css')],
-      },
-    }],
+    ],
   ],
   i18n: {
     defaultLocale: 'en',
@@ -97,12 +108,24 @@ module.exports = {
     localeConfigs: {
       en: {
         htmlLang: 'en-GB',
-        label: 'English'
+        label: 'English',
       },
       es: {
         label: 'Español',
-        htmlLang: 'es-ES'
-      }
+        htmlLang: 'es-ES',
+      },
     },
   },
-};
+  plugins: [
+    async function myPlugin() {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ]
+}
